cd59f607df61d03a375f7b892e0337b487a0a708
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-04-22 21:54:37 +0200

    shell.nix: Add init hook

--- shell.nix
+++ shell.nix
@@ -4,10 +4,16 @@ let
   pkgs = import sources.nixpkgs {};
 in pkgs.mkShell {
   buildInputs = with pkgs; [
+    gitMinimal
     nix-output-monitor
   ];
 
   shellHook = ''
+    init() {
+      git clone gitlab@gitlab.kokakiwi.net:kokakiwi/home-manager-secrets.git secrets
+      switch
+    }
+
     build() {
       nom-build
     }
