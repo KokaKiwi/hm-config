3bacbc2e6d0eb69dcf9cbe0d68f9dc1f1bd5e606
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-04-23 11:28:33 +0200

    programs(glab): Add aliases.yml generation

--- modules/generic/programs/glab.nix
+++ modules/generic/programs/glab.nix
@@ -2,14 +2,27 @@
 with lib;
 let
   cfg = config.programs.glab;
+
+  settingsFormat = pkgs.formats.yaml {};
 in {
   options.programs.glab = {
     enable = mkEnableOption "glab";
 
     package = mkPackageOption pkgs "glab" { };
+
+    aliases = mkOption {
+      type = with types; attrsOf str;
+      default = { };
+    };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
+
+    xdg.configFile = {
+      "glab-cli/aliases.yml" = mkIf (cfg.aliases != { }) {
+        source = settingsFormat.generate "glab-aliases.yml" cfg.aliases;
+      };
+    };
   };
 }
