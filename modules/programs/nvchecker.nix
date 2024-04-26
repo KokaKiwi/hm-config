2f551d3509d91dacef2d476fd326312158906c73
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-04-27 01:37:22 +0200

    programs: nvchecker

--- modules/programs/nvchecker.nix
+++ modules/programs/nvchecker.nix
@@ -1,13 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.nvchecker = {
     package = let
       python = pkgs.python312;
-      nvchecker = python.pkgs.nvchecker;
-    in nvchecker.overridePythonAttrs (super: {
-      propagatedBuildInputs =
-        super.propagatedBuildInputs
-        ++ nvchecker.optional-dependencies.pypi;
-    });
+      pythonEnv = python.withPackages (ps: with ps; [
+        nvchecker
+      ] ++ lib.flatten (builtins.attrValues nvchecker.optional-dependencies));
+    in pkgs.runCommand "nvchecker" { } ''
+      mkdir -p $out/bin
+      for exe in ${pythonEnv}/bin/{nvchecker{,-notify},nvcmp,nvtake}; do
+        ln -s $exe $out/bin/$(basename "$exe")
+      done
+    '';
   };
 }
