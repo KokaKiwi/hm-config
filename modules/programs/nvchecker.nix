518a8568ba7195cb177aaabbdb9c6cab19762dbf
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-04-26 23:56:43 +0200

    programs: Fix nvchecker

--- modules/programs/nvchecker.nix
+++ modules/programs/nvchecker.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 {
   programs.nvchecker = {
-    package = pkgs.python312Packages.nvchecker;
+    package = let
+      python = pkgs.python312;
+      nvchecker = python.pkgs.nvchecker;
+    in nvchecker.overridePythonAttrs (super: {
+      propagatedBuildInputs =
+        super.propagatedBuildInputs
+        ++ nvchecker.optional-dependencies.pypi;
+    });
   };
 }
