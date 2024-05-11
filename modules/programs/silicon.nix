df50f3647a8cb7b613a54ed670a5243b8e2943ae
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-12 01:30:16 +0200

    programs(silicon): Use fenix for compiling

--- modules/programs/silicon.nix
+++ modules/programs/silicon.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   config = {
     programs.silicon = {
       enable = true;
+      package = pkgs.silicon.override {
+        rustPlatform = pkgs.fenixStableRustPlatform;
+        python3 = pkgs.python312;
+      };
     };
   };
 }
