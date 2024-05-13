501e63dd2a10babdb5af18a7064b328e864ddd77
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-13 02:09:56 +0200

    chore(pkgs): Update versions

--- pkgs/applications/cargo-shell/default.nix
+++ pkgs/applications/cargo-shell/default.nix
@@ -1,25 +1,20 @@
-{ lib
-
-, fetchFromGitLab
+{ fetchFromGitLab
 
 , rustPlatform
 }:
-let
-  baseVersion = "0.1.0";
-  rev = "a75033c8fb53e65ff74856ddbcc7ef7bd3b3f715";
-in rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage {
   pname = "cargo-shell";
-  version = lib.gitVersion baseVersion rev;
+  version = "0.1-unstable-2024-04-12";
 
   src = fetchFromGitLab {
     domain = "gitlab.kokakiwi.net";
     owner = "rusted/tools";
     repo = "cargo-shell";
-    inherit rev;
+    rev = "a75033c8fb53e65ff74856ddbcc7ef7bd3b3f715";
     hash = "sha256-RwLXAloNOPQ0LEGpE3/xlsfKdxWYA3QH9JGYhdpgD44=";
   };
 
-  cargoHash = "sha256-HaxrEZ86EwKeYj8AWuASReQlQ9oVNUxvbVSEOSSNquo=";
+  cargoHash = "sha256-ACARVMrYJ+cihPnM/6nGKxdEK9fVYvwKeCaeZqCW/Bs=";
 
   meta = {
     mainProgram = "cargo-shell";
--- pkgs/applications/mux/default.nix
+++ pkgs/applications/mux/default.nix
@@ -7,22 +7,19 @@
 
 , tmux
 }:
-let
-  version = "0.1.10";
-  rev = "ef15f6d1adb505349b86af356590ea138521b9e8";
-in rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage {
   pname = "mux";
-  version = lib.gitVersion version rev;
+  version = "0.1.10-unstable-2024-04-12";
 
   src = fetchFromGitLab {
     domain = "gitlab.kokakiwi.net";
     owner = "rusted/sys";
     repo = "mux";
-    inherit rev;
+    rev = "ef15f6d1adb505349b86af356590ea138521b9e8";
     hash = "sha256-ngT9N6DMp5/TJSP1/387N8YxnOpYU9JGF9P1axeu15w=";
   };
 
-  cargoHash = "sha256-YtQRC0xkCjJs/VM/PGJAeBT2OtQ1Pyv1OwOJGO1JC7k=";
+  cargoHash = "sha256-YKcNsv6cMrBDqo+mM96MICGv4fI9trinzhpLtOvD0uo=";
 
   nativeBuildInputs = [ makeWrapper ];
 
