4aba8564ac54fb39d54c191df768029ba195bb39
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-19 01:49:40 +0200

    programs(nvchecker): fixup

--- modules/programs/nvchecker.nix
+++ modules/programs/nvchecker.nix
@@ -1,16 +1,12 @@
 { pkgs, lib, ... }:
+with lib;
 {
   programs.nvchecker = {
     package = let
       python = pkgs.python312;
-      pythonEnv = python.withPackages (ps: with ps; [
-        nvchecker
-      ] ++ lib.flatten (builtins.attrValues nvchecker.optional-dependencies));
-    in pkgs.runCommand "nvchecker" { } ''
-      mkdir -p $out/bin
-      for exe in ${pythonEnv}/bin/{nvchecker{,-notify},nvcmp,nvtake}; do
-        ln -s $exe $out/bin/$(basename "$exe")
-      done
-    '';
+      nvchecker = python.pkgs.nvchecker;
+    in nvchecker.overridePythonAttrs {
+      dependencies = flatten (attrValues nvchecker.optional-dependencies);
+    };
   };
 }
