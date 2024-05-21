976b415a4ed836ebc4479fd96a09272f8fea0f14
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-21 05:40:38 +0200

    misc(packages): Add trashy

--- modules/misc/packages.nix
+++ modules/misc/packages.nix
@@ -3,11 +3,21 @@ let
   packages = let
     opengl = config.lib.opengl;
   in {
+    cool-retro-term = opengl.wrapPackage pkgs.cool-retro-term;
+    jellyfin-media-player = opengl.wrapPackage pkgs.jellyfin-media-player;
     npins = pkgs.npins.override {
       nix = config.nix.package;
     };
-    jellyfin-media-player = opengl.wrapPackage pkgs.jellyfin-media-player;
-    cool-retro-term = opengl.wrapPackage pkgs.cool-retro-term;
+    trashy = let
+      base = pkgs.trashy.override {
+        rustPlatform = pkgs.fenixStableRustPlatform;
+      };
+    in base.overrideAttrs (super: {
+      postInstall = ''
+        $out/bin/trash manpage > trash.1
+        installManPage trash.1
+      '';
+    });
   };
 in {
   home.packages = with pkgs; [
