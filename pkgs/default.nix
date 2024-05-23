62c2490191dd5be93f8a5b0fa65a8394e0e385cf
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-23 12:53:56 +0200

    pkgs: Fixup overrides

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -71,23 +71,17 @@ let
     }).overrideAttrs (prev: {
       postPatch = prev.postPatch + ''
         substituteInPlace src/main.rs src/{config,core,display,input,git,runtime,todo_file,testutils,view}/src/lib.rs \
-        --replace "warnings" ""
+          --replace-warn "warnings" ""
       '';
     });
 
     glfw3 = super.glfw3.overrideAttrs (super: {
       postPatch = super.postPatch + ''
       substituteInPlace src/wl_init.c \
-      --replace "libdecor-0.so.0" "${pkgs.lib.getLib pkgs.libdecor}/lib/libdecor-0.so.0"
-
-      substituteInPlace src/wl_init.c \
-      --replace "libwayland-client.so.0" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-client.so.0"
-
-      substituteInPlace src/wl_init.c \
-      --replace "libwayland-cursor.so.0" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-cursor.so.0"
-
-      substituteInPlace src/wl_init.c \
-      --replace "libwayland-egl.so.1" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-egl.so.1"
+        --replace-warn "libdecor-0.so.0" "${pkgs.lib.getLib pkgs.libdecor}/lib/libdecor-0.so.0" \
+        --replace-warn "libwayland-client.so.0" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-client.so.0" \
+        --replace-warn "libwayland-cursor.so.0" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-cursor.so.0" \
+        --replace-warn "libwayland-egl.so.1" "${pkgs.lib.getLib pkgs.wayland}/lib/libwayland-egl.so.1"
       '';
     });
 
