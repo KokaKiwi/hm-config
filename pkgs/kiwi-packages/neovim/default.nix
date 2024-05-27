8e67168cb70807881cd6b3760d7443dffd97ccef
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-27 09:53:06 +0200

    pkgs(neovim): Update revision

--- pkgs/kiwi-packages/neovim/default.nix
+++ pkgs/kiwi-packages/neovim/default.nix
@@ -37,13 +37,13 @@ in (neovim-unwrapped.override {
   inherit stdenv;
   inherit lua tree-sitter;
 }).overrideAttrs (final: super: {
-  version = "nightly-unstable-2024-05-26";
+  version = "nightly-unstable-2024-05-27";
 
   src = fetchFromGitHub {
     owner = "neovim";
     repo = "neovim";
-    rev = "05a65432b6bb718d67dda370144e34faf933c086";
-    hash = "sha256-lG8Pu1r73Bn6hYTSHOAnzZkjS6dp9Al8OgJ+gLzfanY=";
+    rev = "9a0239fdc8b380a8a32739a7c722fe90e3c2e910";
+    hash = "sha256-YsiyQzGY//u2qVBH+7Nn31VmafEYDZfuyRz5kbMwHxA=";
   };
 
   inherit tree-sitter;
