1110282bdb978f5d5addf9c2d9c08fe3b856a73e
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-29 18:57:04 +0200

    programs: update nvchecker

--- modules/programs/nvchecker.nix
+++ modules/programs/nvchecker.nix
@@ -3,7 +3,16 @@
   programs.nvchecker = {
     package = let
       python3Packages = pkgs.python312Packages;
-      nvchecker = python3Packages.nvchecker;
+      nvchecker = python3Packages.nvchecker.overridePythonAttrs (super: rec {
+        version = "2.15";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "lilydjwg";
+          repo = "nvchecker";
+          rev = "v${version}";
+          hash = "sha256-MEZvBVsO3JpmyeeB3DECqt2Ynbdf8X8BfNTTUbK3N58=";
+        };
+      });
     in config.lib.python.extendPackageEnv nvchecker (_: lib.flatten (lib.attrValues nvchecker.optional-dependencies));
   };
 }
