896f74cadc01c83cf6232c8764aa1c800dcd1baf
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-06-01 18:52:22 +0200

    pkgs(nvchecker): 2.15 -> 2.15.1

--- modules/programs/nvchecker.nix
+++ modules/programs/nvchecker.nix
@@ -4,13 +4,13 @@
     package = let
       python3Packages = pkgs.python312Packages;
       nvchecker = python3Packages.nvchecker.overridePythonAttrs (super: rec {
-        version = "2.15";
+        version = "2.15.1";
 
         src = pkgs.fetchFromGitHub {
           owner = "lilydjwg";
           repo = "nvchecker";
           rev = "v${version}";
-          hash = "sha256-MEZvBVsO3JpmyeeB3DECqt2Ynbdf8X8BfNTTUbK3N58=";
+          hash = "sha256-dK3rZCoSukCzPOFVectQiF6qplUuDBh9qyN8JL0+j20=";
         };
       });
     in config.lib.python.extendPackageEnv nvchecker (_: lib.flatten (lib.attrValues nvchecker.optional-dependencies));
