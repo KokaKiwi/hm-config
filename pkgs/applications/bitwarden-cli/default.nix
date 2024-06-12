0a57eeff28fb141265fa748bb414624066b580d4
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-06-12 15:05:55 +0200

    pkgs: bitwarden-cli 2024.4.1 -> 2024.6.0

--- pkgs/applications/bitwarden-cli/default.nix
+++ pkgs/applications/bitwarden-cli/default.nix
@@ -8,18 +8,18 @@
 }:
 buildNpmPackage rec {
   pname = "bitwarden-cli";
-  version = "2024.4.1";
+  version = "2024.6.0";
 
   src = fetchFromGitHub {
     owner = "bitwarden";
     repo = "clients";
     rev = "cli-v${version}";
-    hash = "sha256-Dz7EActqXd97kNxEaNINj2O6TLZWEgHHg1lOIa2+Lt4=";
+    hash = "sha256-qiUUrs23WHE3+KFsWDknuDSA6M3Zwjz9Jdjq6mn5XkE=";
   };
 
   inherit nodejs;
 
-  npmDepsHash = "sha256-fjYez3nSDsG5kYtrun3CkDCz1GNAjNlwPzEL+/9qQRU=";
+  npmDepsHash = "sha256-Mgd15eFJtWoBqFFCsjmsnlNbcg5NDs1U7DlMkE0hIb8=";
 
   nativeBuildInputs = [
     python3
@@ -29,7 +29,7 @@ buildNpmPackage rec {
 
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-  npmBuildScript = "build:prod";
+  npmBuildScript = "build:oss:prod";
 
   npmWorkspace = "apps/cli";
 
