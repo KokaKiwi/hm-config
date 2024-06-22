b472bc41591ca15ab8d5d85745179e0ad91c79c2
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-06-22 19:03:20 +0200

    lib.wrapPackage: Replace package references in *.desktop files

--- modules/lib/package.nix
+++ modules/lib/package.nix
@@ -23,6 +23,17 @@
           cp -rs --no-preserve=mode ${drv.${outputName}} ''$${outputName}
         '') (super.outputs or [ "out" ])}
 
+        # Replaces package references in .desktop files
+        for f in $out/share/applications/*.desktop; do
+          if ! grep -q "${drv.out}" "$f"; then
+            continue
+          fi
+
+          src="$(readlink "$f")"
+          rm "$f"
+          sed "s|${drv.out}|$out|g" "$src" > "$f"
+        done
+
         ${buildCommand}
       '';
     });
