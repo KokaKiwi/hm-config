b9646674bf2439457b7ac9c989f7a715beca0916
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-06-29 23:05:23 +0200

    pkgs.minio-client: 2024-06-24T19-40-33Z -> 2024-06-29T19-08-46Z
    Diff: https://github.com/minio/mc/compare/RELEASE.2024-06-24T19-40-33Z...RELEASE.2024-06-29T19-08-46Z

--- pkgs/applications/tools/networking/minio-client/default.nix
+++ pkgs/applications/tools/networking/minio-client/default.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "minio-client";
-  version = "2024-06-24T19-40-33Z";
+  version = "2024-06-29T19-08-46Z";
 
   src = fetchFromGitHub {
     owner = "minio";
     repo = "mc";
     rev = "RELEASE.${version}";
-    sha256 = "sha256-ufqNSehBEJbZ/6/1kujoSZCpXD1ClAVXU8IBWvOUKao=";
+    sha256 = "sha256-fZyCtn2YAklmkSwxTkMSKmZ8C88z3pWnAKto47zP2qw=";
   };
 
   vendorHash = "sha256-OlfFmcBet9D0EQWSSsXkvy0dZxMqX7g+4KqNK79N650=";
