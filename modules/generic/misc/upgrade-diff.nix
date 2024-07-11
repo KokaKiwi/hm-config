cee454f2aaffc14dfaff1998753cd55762793f20
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-07-11 13:57:33 +0200

    modules(upgrade-diff): Fixup

--- modules/generic/misc/upgrade-diff.nix
+++ modules/generic/misc/upgrade-diff.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 {
-  home.activation.diff = with lib; hm.dag.entryAfter [ "installPackages" ] ''
+  home.activation.diff = with lib; hm.dag.entryAfter [ "installPackages" "importGpgKeys" ] ''
     if [[ -e "$oldGenPath" ]]; then
       ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff "$oldGenPath" "$newGenPath"
     fi
