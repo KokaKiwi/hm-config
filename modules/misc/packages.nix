0c974766ce126f014fcc8c0914f90688f5855238
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-07-19 13:01:23 +0200

    packages: Add nix-prefetch

--- modules/misc/packages.nix
+++ modules/misc/packages.nix
@@ -52,7 +52,7 @@ in {
     ponysay xinspect
     trashy minisign mkcert doggo
     nix-binutils git-absorb pingu miniserve
-    patool
+    patool nix-prefetch
     nixgl.nixGLIntel
     kiwiPackages.doll
     nur.repos.kokakiwi.go-mod-upgrade
