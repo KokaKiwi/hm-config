3e8898a5a0d59daec69f1c71ab4606b8640731fa
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-08-09 03:22:17 +0200

    pkgs.cargo-shell: 0.1-unstable-2024-04-12 -> 0-unstable-2024-08-09
    Diff: https://gitlab.kokakiwi.net/rusted/tools/cargo-shell/-/compare/a75033c8fb53e65ff74856ddbcc7ef7bd3b3f715...0a3a2ae6393688f9c24deef9510e4df523a48f48

--- pkgs/applications/cargo-shell/default.nix
+++ pkgs/applications/cargo-shell/default.nix
@@ -4,17 +4,17 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "cargo-shell";
-  version = "0.1-unstable-2024-04-12";
+  version = "0-unstable-2024-08-09";
 
   src = fetchFromGitLab {
     domain = "gitlab.kokakiwi.net";
     owner = "rusted/tools";
     repo = "cargo-shell";
-    rev = "a75033c8fb53e65ff74856ddbcc7ef7bd3b3f715";
-    hash = "sha256-RwLXAloNOPQ0LEGpE3/xlsfKdxWYA3QH9JGYhdpgD44=";
+    rev = "0a3a2ae6393688f9c24deef9510e4df523a48f48";
+    hash = "sha256-o5MQp1mlUBKFYSssF+Q4CV84AEwEGylEa1WkpUgPyB0=";
   };
 
-  cargoHash = "sha256-ACARVMrYJ+cihPnM/6nGKxdEK9fVYvwKeCaeZqCW/Bs=";
+  cargoHash = "sha256-YrpZhNtA/yAejL6Dn6OSsb3CohRCMhXgroQNP1tFvCc=";
 
   meta = {
     mainProgram = "cargo-shell";
