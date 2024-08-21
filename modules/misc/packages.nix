273efa61f25c0146eb2265914204933611c4b8bc
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-08-21 15:52:39 +0200

    homePackages.bun: 1.1.24 -> 1.1.25
    Diff: https://github.com/oven-sh/bun/compare/None...1.1.25
    
    Changelog: https://bun.sh/blog/bun-v1.1.20

--- modules/misc/packages.nix
+++ modules/misc/packages.nix
@@ -20,13 +20,13 @@ let
     bun = let
       bun = pkgs.bun;
     in bun.overrideAttrs (self: super: {
-      version = "1.1.24";
+      version = "1.1.25";
 
       buildInputs = [ ];
 
       src = pkgs.fetchurl {
         url = "https://github.com/oven-sh/bun/releases/download/bun-v${self.version}/bun-linux-x64.zip";
-        hash = "sha256-9HiIBnjPKC/dpGzJ1Y4goN3h+tjJz+4Sjq1DmWRT4cg=";
+        hash = "sha256-5w/frKlIFjgBEtDJHstoxCPmhmL77xSOukPo7nO5Rsk=";
       };
     });
     cool-retro-term = opengl.wrapPackage pkgs.cool-retro-term;
