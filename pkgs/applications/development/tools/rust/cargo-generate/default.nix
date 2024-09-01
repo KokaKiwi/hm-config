39a29776a39d7eadfc57e6083eb612deedf1d40e
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-09-01 22:51:04 +0200

    pkgs.cargo-generate.local: 0.21.3 -> 0.22.0
    Diff: https://github.com/cargo-generate/cargo-generate/compare/v0.21.3...v0.22.0
    
    Changelog: https://github.com/cargo-generate/cargo-generate/blob/v0.22.0/CHANGELOG.md

--- pkgs/applications/development/tools/rust/cargo-generate/default.nix
+++ pkgs/applications/development/tools/rust/cargo-generate/default.nix
@@ -10,19 +10,18 @@
 
 , git
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "cargo-generate";
-  version = "0.21.3";
+  version = "0.22.0";
 
   src = fetchFromGitHub {
     owner = "cargo-generate";
     repo = "cargo-generate";
     rev = "v${version}";
-    sha256 = "sha256-1F/865UgdqwfpITFhXCuL7CmducL7w0lVDyfui9UzjU=";
+    sha256 = "sha256-oiXv6MbQpmWFi2cTN3a1Zx7Bjr0Y+f6/O+0FQNidbBg=";
   };
 
-  cargoHash = "sha256-szPO1V09EThpo2N03Ll+ZJUpvjp2b+/C/sviOzFfG+k=";
+  cargoHash = "sha256-8yLGxydU7jjoG13I+h7qjtabcCxzjnEiE8tAbH56pp4=";
 
   nativeBuildInputs = [ pkg-config ];
 
@@ -42,9 +41,16 @@ rustPlatform.buildRustPackage rec {
   # Exclude some tests that don't work in sandbox:
   # - favorites_default_to_git_if_not_defined: requires network access to github.com
   # - should_canonicalize: the test assumes that it will be called from the /Users/<project_dir>/ folder on darwin variant.
-  checkFlags = [
-    "--skip=favorites::favorites_default_to_git_if_not_defined"
-  ];
+  checkFlags = let
+    skipTests = [
+      "favorites::favorites_default_to_git_if_not_defined"
+      "git_instead_of::should_read_the_instead_of_config_and_rewrite_an_git_at_url_to_https"
+      "git_instead_of::should_read_the_instead_of_config_and_rewrite_an_ssh_url_to_https"
+      "git_over_ssh::it_should_retrieve_the_private_key_from_ssh_agent"
+      "git_over_ssh::it_should_support_a_public_repo"
+      "git_over_ssh::it_should_use_a_ssh_key_provided_by_identity_argument"
+    ];
+  in map (name: "--skip=${name}") skipTests;
 
   env = {
     LIBGIT2_NO_VENDOR = 1;
