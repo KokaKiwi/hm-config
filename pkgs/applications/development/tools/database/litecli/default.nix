28c6c517fcb95da1aef71791a9a3cb70158203eb
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-09-11 14:03:00 +0200

    pkgs.litecli.local: 1.12.2 -> 1.12.3
    Diff: https://github.com/dbcli/litecli/compare/v1.12.2...v1.12.3
    
    Changelog: https://github.com/dbcli/litecli/blob/v1.12.3/CHANGELOG.md

--- pkgs/applications/development/tools/database/litecli/default.nix
+++ pkgs/applications/development/tools/database/litecli/default.nix
@@ -6,14 +6,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "litecli";
-  version = "1.12.2";
+  version = "1.12.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dbcli";
     repo = "litecli";
     rev = "v${version}";
-    hash = "sha256-6AtpLjPAzlioBnAbyfTqvf/BUfCMQ23GO1ijnL3286w=";
+    hash = "sha256-TPwzXfb4n6wTe6raQ5IowKdhGkKrf2pmSS2+Q03NKYk=";
   };
 
   build-system = with python3Packages; [
