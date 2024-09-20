bf19da32211ab7d3db55de6c3df5cd25410d3b9c
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-09-20 13:04:08 +0200

    sources: Fixups

--- sources.nix
+++ sources.nix
@@ -1,12 +1,19 @@
 let
   system = builtins.currentSystem;
 
+  pkgs = import main.nixpkgs { inherit system; };
+  inherit (pkgs) lib callPackage;
+
   patches = {
     agenix = [
       ./npins/patches/agenix/0001-Fix-rekey.patch
     ];
     catppuccin = [
       ./npins/patches/catppuccin/0001-Expose-lib.ctp.patch
+      (pkgs.fetchpatch {
+        url = "https://github.com/catppuccin/nix/pull/337.patch";
+        hash = "sha256-/p86ah92DVj3CfwuAySvET+5b9G+a9nebE4diJiMCU4=";
+      })
     ];
     home-manager = [
       ./npins/patches/home-manager/0001-PR-4801-Add-a-podman-module-for-containers-and-netwo.patch
@@ -16,9 +23,6 @@ let
   main = import ./npins;
   nur = import ./npins/nur;
   channels = import ./npins/channels;
-
-  pkgs = import main.nixpkgs { inherit system; };
-  inherit (pkgs) lib callPackage;
 
   lix = callPackage ./pkgs/lix.nix { };
 
