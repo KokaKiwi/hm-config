4b371b5f53d3801cc9dd6a58629443dda2546653
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-10-08 18:15:33 +0200

    pkgs.bun: 1.1.29 -> 1.1.30
    Diff: https://github.com/oven-sh/bun/compare/None...1.1.30
    
    Changelog: https://bun.sh/blog/bun-v1.1.30

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -2,11 +2,11 @@
 {
   # Udated packages
   bun = super.bun.overrideAttrs (self: prev: {
-    version = "1.1.29";
+    version = "1.1.30";
 
     src = pkgs.fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${self.version}/bun-linux-x64.zip";
-      hash = "sha256-RnKczYB/IkUYVBnRktCFhHsmvObQovVMfCilqJq3q1g=";
+      hash = "sha256-UGDyM+7L7YGXaGs3sOstqP9SKC9XvR3Y45T1Ov3shA4=";
     };
   });
 }
