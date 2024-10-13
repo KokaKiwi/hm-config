66ba3623f44705337f57e69c14eba1a4fb91f3ae
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-10-13 03:16:07 +0200

    hosts.kira.programs.xh: Use OpenSSL 3.3

--- hosts/kira/programs/xh.nix
+++ hosts/kira/programs/xh.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.xh = {
     enable = true;
+    package = pkgs.xh.override {
+      withNativeTls = true;
+      openssl = pkgs.openssl_3_3;
+    };
 
     settings = {
       defaultOptions = [
