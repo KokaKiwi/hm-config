994235003889b05a533e1dd4f68853eddfd156b5
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-10-20 10:46:14 +0200

    pkgs.cargo-make.local: 0.37.22 -> 0.37.23
    Diff: https://github.com/sagiegurari/cargo-make/compare/0.37.22...0.37.23
    
    Changelog: https://github.com/sagiegurari/cargo-make/blob/0.37.23/CHANGELOG.md

--- pkgs/applications/development/tools/rust/cargo-make/default.nix
+++ pkgs/applications/development/tools/rust/cargo-make/default.nix
@@ -10,16 +10,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-make";
-  version = "0.37.22";
+  version = "0.37.23";
 
   src = fetchFromGitHub {
     owner = "sagiegurari";
     repo = "cargo-make";
     rev = version;
-    hash = "sha256-j2NhQZynKtYUjwohAT0x0VF5xhKqXJsbd71y1Sf9+A4=";
+    hash = "sha256-yYZasrnfxpLf0z6GndLYhkIFfVNjTkx4zdfHYX6WyXk=";
   };
 
-  cargoHash = "sha256-JEYrChYHLPQAi1BySrOzRtPKwd0Tmh84LWwwwbKUSdk=";
+  cargoHash = "sha256-X4FhUqhf58wbl3A8nlXAqnYkkaXajxxqRyJn5K7BVqM=";
 
   nativeBuildInputs = [ pkg-config ];
 
