64c4d7e0b2245e52a3bfe9c3ce0ea38cd46260bc
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-05 15:14:35 +0100

    pkgs.cargo-expand.local: 1.0.94 -> 1.0.95
    Diff: https://github.com/dtolnay/cargo-expand/compare/1.0.94...1.0.95
    
    Changelog: https://github.com/dtolnay/cargo-expand/releases/tag/1.0.95

--- pkgs/applications/development/tools/rust/cargo-expand/default.nix
+++ pkgs/applications/development/tools/rust/cargo-expand/default.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-expand";
-  version = "1.0.94";
+  version = "1.0.95";
 
   src = fetchFromGitHub {
     owner = "dtolnay";
     repo = pname;
     rev = version;
-    hash = "sha256-IraGvos7t065C4NWCbUmIkFWEUgVgPydD8KJ1OK27YU=";
+    hash = "sha256-VEjgSmZcy/CZ8EO/mJ2nBOpQviF4A/QQ8SpLLF/9x4c=";
   };
 
-  cargoHash = "sha256-y5z+1zCraXfjGviJi76fG4hG2Y23nUZZFSVBsZPerwE=";
+  cargoHash = "sha256-m/F6fI1d8i5lVyURti86FWAs/U14TXpgg/nemLAv4NI=";
 
   meta = with lib; {
     description = "Cargo subcommand to show result of macro expansion";
