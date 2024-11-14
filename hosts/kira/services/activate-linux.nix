48c7f0b0118be2e1c62ff80d66d0c13ab70a84bf
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-14 17:04:21 +0100

    hosts.kira.services.activate-linux: Update message

--- hosts/kira/services/activate-linux.nix
+++ hosts/kira/services/activate-linux.nix
@@ -8,7 +8,7 @@
 
     config = {
       text-title = "Activate Nix/Arch::OS";
-      text-message = "8098 is a good bot";
+      text-message = "{42999158-ae9f-5b44-8834-27675aacf427}";
 
       scale = 1.3;
     };
