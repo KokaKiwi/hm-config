33bc40bbc63ea0e04e9a8cbf00880d5da3e5019f
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-18 23:41:21 +0100

    pkgs.nix-update.local: 1.6.0 -> 1.7.0
    Diff: https://github.com/Mic92/nix-update/compare/1.6.0...1.7.0
    
    Changelog: https://github.com/Mic92/nix-update/releases/tag/1.7.0

--- pkgs/applications/tools/package-management/nix-update/default.nix
+++ pkgs/applications/tools/package-management/nix-update/default.nix
@@ -11,14 +11,14 @@
 }:
 python3.pkgs.buildPythonApplication rec {
   pname = "nix-update";
-  version = "1.6.0";
+  version = "1.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "nix-update";
     rev = version;
-    hash = "sha256-CpvLZ5cXNkIbAZjO1HebFaOiLyeesnkQxJ95V0anaII=";
+    hash = "sha256-5KhS3RxeVDHKjsxT3hHtl0hHcwlSHl2fYFYLonobpLg=";
   };
 
   nativeBuildInputs = [
