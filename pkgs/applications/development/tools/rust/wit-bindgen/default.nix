ff1688b9b9e72e70bd0f7a937d5a2eba96a9fba8
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-27 23:01:32 +0100

    pkgs.wit-bindgen.local: 0.35.0 -> 0.36.0
    Diff: https://github.com/bytecodealliance/wit-bindgen/compare/v0.35.0...v0.36.0

--- pkgs/applications/development/tools/rust/wit-bindgen/default.nix
+++ pkgs/applications/development/tools/rust/wit-bindgen/default.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "wit-bindgen";
-  version = "0.35.0";
+  version = "0.36.0";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "wit-bindgen";
     rev = "v${version}";
-    hash = "sha256-bxA+SsuxDbeORbuul2A0+a2tD0m8IPG5mCekKiHv+uw=";
+    hash = "sha256-Ebg5llC7w2YoHqs3UatZbC5gVi6L9zGuEkzqhJhGXh4=";
   };
 
-  cargoHash = "sha256-Wujmj/HArLy7s5ZqZlOZssE9CxsXzkM97N61GVb6M88=";
+  cargoHash = "sha256-NgKxDUYWMib0+HpuuXh2znRcYVVo30VRXPHPIkW6rpk=";
 
   # Some tests fail because they need network access to install the `wasm32-unknown-unknown` target.
   # However, GitHub Actions ensures a proper build.
