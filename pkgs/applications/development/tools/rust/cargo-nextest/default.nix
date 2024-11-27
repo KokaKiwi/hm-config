ea3dda536b3984256ddd1d0eb4ec96e443e8c955
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-27 01:14:21 +0100

    pkgs.cargo-nextest.local: 0.9.84 -> 0.9.85
    Diff: https://github.com/nextest-rs/nextest/compare/cargo-nextest-0.9.84...cargo-nextest-0.9.85
    
    Changelog: https://nexte.st/CHANGELOG.html

--- pkgs/applications/development/tools/rust/cargo-nextest/default.nix
+++ pkgs/applications/development/tools/rust/cargo-nextest/default.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-nextest";
-  version = "0.9.84";
+  version = "0.9.85";
 
   src = fetchFromGitHub {
     owner = "nextest-rs";
     repo = "nextest";
     rev = "cargo-nextest-${version}";
-    hash = "sha256-27ZGA30uYAP0MpxJASEPXq189KzbzN54cEbIM8QbSuY=";
+    hash = "sha256-ti4sVtlBiggwrt+xiOMCwjKfxUDkzRyCi8JjwXveQm8=";
   };
 
-  cargoHash = "sha256-Y/t6gCpCmKM6nm+qyMAPX+Yzyp4m/dmRs+cR3Dk2KdM=";
+  cargoHash = "sha256-x8aTcX82O+Pie8qPsox61D2cJBdsYWEyIEnkj5Qu+vA=";
 
   cargoBuildFlags = [ "-p" "cargo-nextest" ];
   cargoTestFlags = [ "-p" "cargo-nextest" ];
