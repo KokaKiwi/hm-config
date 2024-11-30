7480153b4764acf02b53c5d31c7adb463d6f37ac
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-11-30 11:49:55 +0100

    hosts.kira.config.programs.nvchecker.package: 2.15.1 -> 2.16
    Diff: https://github.com/lilydjwg/nvchecker/compare/v2.15.1...v2.16
    
    Changelog: https://github.com/lilydjwg/nvchecker/releases/tag/v2.15.1

--- hosts/kira/programs/nvchecker.nix
+++ hosts/kira/programs/nvchecker.nix
@@ -5,13 +5,13 @@
     package = let
       python3Packages = pkgs.python312Packages;
       nvchecker = python3Packages.nvchecker.overridePythonAttrs (super: rec {
-        version = "2.15.1";
+        version = "2.16";
 
         src = pkgs.fetchFromGitHub {
           owner = "lilydjwg";
           repo = "nvchecker";
           rev = "v${version}";
-          hash = "sha256-dK3rZCoSukCzPOFVectQiF6qplUuDBh9qyN8JL0+j20=";
+          hash = "sha256-HdL3BnjQZzKXtjhQqDst6dJH82g3BONFsGUnwzDMRDA=";
         };
       });
     in config.lib.python.extendPackageEnv nvchecker (_: lib.flatten (lib.attrValues nvchecker.optional-dependencies));
