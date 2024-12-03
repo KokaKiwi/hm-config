6f33a9efde55bf5f27a085995d8eb5d95f9e84f6
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-12-03 20:29:57 +0100

    pkgs.gleam.local: 1.6.2 -> 1.6.3
    Diff: https://github.com/gleam-lang/gleam/compare/refs/tags/v1.6.2...v1.6.3

--- pkgs/applications/compilers/gleam/default.nix
+++ pkgs/applications/compilers/gleam/default.nix
@@ -10,20 +10,20 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "gleam";
-  version = "1.6.2";
+  version = "1.6.3";
 
   src = fetchFromGitHub {
     owner = "gleam-lang";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-r+iN6eLmy6qnrhvHKUQufd/4mvZL9TCVaqLqEPcLu1E=";
+    hash = "sha256-o0ArMXt/APouJTQN4e9W0psm3Tnutq74fMFoM35tP4E=";
   };
 
   nativeBuildInputs = [ gitMinimal pkg-config ];
 
   buildInputs = [ openssl ];
 
-  cargoHash = "sha256-+kKKdNAvr8sTaFvzCCpY1bVS779DgJbHI9kUUIi4lXE=";
+  cargoHash = "sha256-pjLIba4fplJIUnQXmtZ41ywthHSbBpFRPK1qjVrW+fY=";
 
   meta = with lib; {
     description = "A statically typed language for the Erlang VM";
