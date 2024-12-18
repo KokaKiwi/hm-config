717a03be25cc5a807846763f067cdb42f4370ff5
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-12-18 21:03:27 +0100

    pkgs.usage.local: 1.6.0 -> 1.7.2
    Diff: https://github.com/jdx/usage/compare/v1.6.0...v1.7.2

--- pkgs/applications/tools/usage/default.nix
+++ pkgs/applications/tools/usage/default.nix
@@ -1,16 +1,16 @@
 { fetchFromGitHub, rustPlatform}:
 rustPlatform.buildRustPackage rec {
   pname = "usage";
-  version = "1.6.0";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "jdx";
     repo = "usage";
     rev = "v${version}";
-    hash = "sha256-tKwJYVQYNh6m50Dx/s8KSS4qSU6JYnurL33RWX5g2ow=";
+    hash = "sha256-VeSe6WV1Tpy1PQ33EbgmFi6YWWxz3bQksW06sTHTuJU=";
   };
 
-  cargoHash = "sha256-5hfBMglFetEzv0nsLbqYHr15fIwctchbzqtT+0fJI6U=";
+  cargoHash = "sha256-Pn5tUublRo37nBLM1baQ4ey0KL7iAilpHrB/caOSK0I=";
 
   checkFlags = [
     "--skip=complete_word_mounted"
