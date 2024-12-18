f02889ba4623ed99d2610948cf969109b7bdce08
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-12-18 12:40:03 +0100

    pkgs.uv.local: 0.5.9 -> 0.5.10
    Diff: https://github.com/astral-sh/uv/compare/refs/tags/0.5.9...0.5.10
    
    Changelog: https://github.com/astral-sh/uv/blob/refs/tags/0.5.10/CHANGELOG.md

--- pkgs/applications/development/tools/uv/default.nix
+++ pkgs/applications/development/tools/uv/default.nix
@@ -16,21 +16,21 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "uv";
-  version = "0.5.9";
+  version = "0.5.10";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "uv";
     rev = "refs/tags/${version}";
-    hash = "sha256-a6srcSOUoQ5+rtVLxyMNUUU/nO9fO7kNUFN2so0kmxk=";
+    hash = "sha256-GE/MgaX6JhzVVwrkz33fr1Vl83QD1WHhlB7vPdJ2W3c=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "async_zip-0.0.17" = "sha256-VfQg2ZY5F2cFoYQZrtf2DHj0lWgivZtFaFJKZ4oyYdo=";
-      "pubgrub-0.2.1" = "sha256-zusQxYdoNnriUn8JCk5TAW/nQG7fwxksz0GBKEgEHKc=";
+      "pubgrub-0.2.1" = "sha256-bv5QtBFMzXR0Do6wxigxT6QCJPagX2xZE88CmxdISU4=";
       "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
     };
   };
