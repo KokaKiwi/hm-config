6dcf98c25587241304eb29b54d87b08a9aac548d
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-12-19 10:22:02 +0100

    pkgs.nomad_1_9.local: 1.9.3 -> 1.9.4
    Diff: https://github.com/hashicorp/nomad/compare/v1.9.3...v1.9.4

--- pkgs/applications/networking/cluster/nomad/1_9.nix
+++ pkgs/applications/networking/cluster/nomad/1_9.nix
@@ -3,9 +3,9 @@
 , buildGo123Module
 }:
 callPackage ./generic.nix {
-  version = "1.9.3";
-  srcHash = "sha256-KjVr9NIL9Qw10EoP/C+2rjtqU2qBSF6SKpIvQWQJWuo=";
-  vendorHash = "sha256-paUI5mYa9AvMsI0f/VeVdnZzwKS9gsBIb6T4KmugPKQ=";
+  version = "1.9.4";
+  srcHash = "sha256-yhOyHfD5099dCY7lIJzTb8tQrqQ86N8HVtSk5mB7saY=";
+  vendorHash = "sha256-Cxjgs4Hmv0sq11OpvYLnNcc76ojwqwcxMmCZT5Or0f4=";
 
   buildGoModule = buildGo123Module;
 }
