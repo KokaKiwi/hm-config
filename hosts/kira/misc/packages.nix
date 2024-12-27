9553ced35f44dfbc4ea7c68bc76ae713deca3252
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-12-27 01:58:04 +0100

    hosts.kira.packages: Stuff

--- hosts/kira/misc/packages.nix
+++ hosts/kira/misc/packages.nix
@@ -19,7 +19,7 @@ let
     cool-retro-term = opengl.wrapPackage pkgs.cool-retro-term { };
     imhex = let
       package = pkgs.nur.repos.kokakiwi.imhex.override {
-        inherit (pkgs.kiwiPackages) llvm;
+        inherit (pkgs.kiwiPackages) llvmPackages;
         python3 = pkgs.python312;
       };
     in opengl.wrapPackage package { };
